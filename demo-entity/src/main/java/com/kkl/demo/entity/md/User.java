5c0c56c466618b36ef5578f952951e73b3ac3b9e
jeffzhao1985 <jeff.mx.zhao@outlook.com>
2018-01-03 10:12:54 +0800

    init

---
+++ demo-entity/src/main/java/com/kkl/demo/entity/md/User.java
@@ -0,0 +1,30 @@
+package com.kkl.demo.entity.md;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel
+public class User {
+    @Getter
+    @Setter
+    @ApiModelProperty(name = "用户ID", notes = "用户ID", required = true)
+    private Long id;
+
+    @Getter
+    @Setter
+    @ApiModelProperty(name = "用户姓名", notes = "用户姓名", required = true)
+    private String name;
+
+    @Getter
+    @Setter
+    @ApiModelProperty(name = "用户年龄", notes = "用户年龄")
+    private Integer age;
+
+    @Getter
+    @Setter
+    @ApiModelProperty(name = "用户性别", notes = "用户性别")
+    private String gender;
+}
